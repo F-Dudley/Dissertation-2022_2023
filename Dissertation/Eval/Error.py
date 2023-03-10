2d637f85252adcaa699d19cf648cea5a6ef76240
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2023-03-10 14:57:20 +0000

    Implemented RMSE Functionality, Checking Different Outputs

--- Dissertation/Eval/Error.py
+++ Dissertation/Eval/Error.py
@@ -1,32 +1,6 @@
+import sys
 import open3d as o3d
 import numpy as np
-
-
-def OLD_CalculateRMSE(cloud1: o3d.geometry.PointCloud, cloud2: o3d.geometry.PointCloud) -> float:
-    """Calculates the root mean squared error between two point clouds."""
-
-    # Get the points from the clouds
-    points1 = np.asarray(cloud1.points)
-    points2 = np.asarray(cloud2.points)
-
-    assert points1.shape == points2.shape
-
-    # Calculate the difference between the points
-    difference = points1 - points2
-
-    # Calculate the squared difference
-    squaredDifference = difference * difference
-
-    # Sum the squared difference
-    sumSquaredDifference = np.sum(squaredDifference)
-
-    # Divide the sum by the number of points
-    meanSquaredDifference = sumSquaredDifference / points1.shape[0]
-
-    # Take the square root of the mean
-    rootMeanSquaredDifference = np.sqrt(meanSquaredDifference)
-
-    return rootMeanSquaredDifference
 
 
 def CalculateRMSE(cloud1: o3d.geometry.PointCloud, cloud2: o3d.geometry.PointCloud) -> float:
@@ -39,3 +13,27 @@ def CalculateRMSE(cloud1: o3d.geometry.PointCloud, cloud2: o3d.geometry.PointClo
     rmse = np.sqrt(mse)
 
     return rmse
+
+
+def CalculateRMSE_brute(baseCloud: o3d.geometry.PointCloud, evalCloud: o3d.geometry.PointCloud) -> float:
+    """Calculates the root mean squared error between two point clouds."""
+
+    sum = 0
+
+    for point in evalCloud.points:
+
+        closestDist: np.floating = np.finfo(np.float32).max
+
+        for point2 in baseCloud.points:
+
+            newDist = np.linalg.norm(point - point2)
+            if newDist < closestDist:
+                closestDist = newDist
+
+        sqDist = np.square(closestDist)
+
+        sum += sqDist
+
+    mse = sum / len(evalCloud.points)
+
+    return np.sqrt(mse)
