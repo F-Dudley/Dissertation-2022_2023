2dafdb14d48f2ddc5062d8e378e551035ad5d738
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2023-05-14 00:23:46 +0100

    Fixed Registration Demo

---
+++ www/src/utils/three.ts
@@ -0,0 +1,30 @@
+import type { BufferAttribute } from 'three';
+
+export const DemoSphereLines = (
+	spherePosAttr1: BufferAttribute,
+	spherePosAttr2: BufferAttribute,
+): Float32Array => {
+	if (
+		!spherePosAttr1 ||
+		!spherePosAttr2 ||
+		spherePosAttr1.count !== spherePosAttr2.count
+	) {
+		throw new Error('Invalid or Incompatible geometries');
+	}
+
+	const arrLen = (spherePosAttr1.count + spherePosAttr2.count) * 3;
+	const returnArr = new Float32Array(arrLen);
+
+	for (let index = 0; index < spherePosAttr1.count; index++) {
+		const arrIndex = index * 6;
+
+		returnArr[arrIndex] = spherePosAttr1.getX(index);
+		returnArr[arrIndex + 1] = spherePosAttr1.getY(index);
+		returnArr[arrIndex + 2] = spherePosAttr1.getZ(index);
+		returnArr[arrIndex + 3] = spherePosAttr2.getX(index);
+		returnArr[arrIndex + 4] = spherePosAttr2.getY(index);
+		returnArr[arrIndex + 5] = spherePosAttr2.getZ(index);
+	}
+
+	return returnArr;
+};
