bfbaed4cbd277525a074b944e47565c6cb5607f8
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2023-05-17 17:57:07 +0100

    Fixed Starting Issues / Action Secret

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,19 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	server: {
-		port: 3000,
-	},
-	base: '/Dissertation-2022_2023/',
-	plugins: [
-		react(),
-		tsconfigPaths(),
-		Pages({
-			extensions: ['tsx', 'jsx'],
-		}),
-	],
-});
+export default ({ mode }: {mode: string}) => {
+
+	const env = loadEnv(mode, process.cwd(), 'SERVER_');
+
+	const isProduction = mode === 'production';
+
+	const basePath = isProduction ? env.SERVER_BASE_PATH : '/';
+
+	console.log('Using Base Path: ', basePath)
+	console.log('Using Mode: ', mode)
+
+	return defineConfig({
+		base: '/',
+		server: {
+			port: 3000,
+		},
+		plugins: [
+			react(),
+			tsconfigPaths(),
+			Pages({
+				extensions: ['tsx', 'jsx'],
+			}),
+		],
+	});
+};
